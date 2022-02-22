98e8118e1a2956dcdcfea3178b17bf6221852900
faoberilo <89050385+faoberilo@users.noreply.github.com>
2022-02-22 14:58:25 -0300

    Update usuario.controller.ts

--- src/usuario/usuario.controller.ts
+++ src/usuario/usuario.controller.ts
@@ -23,8 +23,7 @@ export class UsuarioController {
   constructor(private readonly usuarioService: UsuarioService) {}
 
   @Post()
-  //@UseGuards(AuthGuard("jwt"), RolesGuard)
-  //@Roles(Role.Admin)
+ 
   create(@Body() createUsuarioDto: CreateUsuarioDto) {
     return this.usuarioService.create(createUsuarioDto);
   }
